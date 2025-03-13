ab061e687c5a43dfe2d70006001fd1f4f9543937
BarryTheBee571 <yuan.zhuang9@education.nsw.gov.au>
2025-03-13 14:41:29 +1100

    ifnished calculator

---
+++ calculator.py
@@ -0,0 +1,6 @@
+import math_operations
+print("What two numbers would you like to add together?")
+a = int(input("What would you like as your first number?"))
+b = int(input("What would you like as your second number?"))
+
+print(f"The answer is", math_operations.addnumbers(a,b))
